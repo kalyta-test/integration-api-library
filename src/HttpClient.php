106741b301174fbf627b70a666ed1a69b6f732c3
kalyta-test <liuba.kalyta@gmail.com>
2023-10-23 17:27:55 +0300

    Add httpClient

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -1,1 +1,24 @@
 <?php
+
+namespace Kalyta\IntegrationApiLibrary;
+
+class HttpClient
+{
+    const CLIENT_HOST = 'https://dev-api.rafinita.com';
+    protected $client;
+
+    public function makeClient()
+    {
+        $client = new \GuzzleHttp\Client(
+            [
+                'base_uri' => self::CLIENT_HOST,
+                'headers' => [
+                    'Accept' => 'application/json'
+                ]
+            ]
+        );
+        $response = $client->post('/post', []);
+
+        return $response->getStatusCode();
+    }
+}
